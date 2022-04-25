41e07353f8f11fac05f378f4a59183bfe5ed56aa
Pleskyjp <Plesky.jp@gmail.com>
2022-04-25 21:29:09 +0200

    completed 2-3 exc

--- excercises_js_basic/excercises 3/excercise_2/exc-2-3.js
+++ excercises_js_basic/excercises 3/excercise_2/exc-2-3.js
@@ -1,6 +1,5 @@
-
 function isPalindrom(word) {
-    //TODO
+  return word === word.split('').reverse().join('');
 }
 
-console.log(isPalindrom("ohoho")); //true
+console.log(isPalindrom('ohoho')); //true
