09072a1b1b4f684c756a598a182d33a84eb2c158
Pleskyjp <Plesky.jp@gmail.com>
2022-04-25 21:14:08 +0200

    completing 2-1 exc

--- excercises_js_basic/excercises 3/excercise_2/exc-2-1.js
+++ excercises_js_basic/excercises 3/excercise_2/exc-2-1.js
@@ -1,6 +1,5 @@
-
 function camelToSnake(camel) {
-    //TODO
+  return camel.replace(/([A-Z]+)/g, (letter) => `_${letter.toLowerCase()}`);
 }
 
-console.log(camelToSnake("someNewWordToTransformAndA")); //some_new_word_to_transform_and_a
+console.log(camelToSnake('someNewWordToTransformAndA')); //some_new_word_to_transform_and_a
