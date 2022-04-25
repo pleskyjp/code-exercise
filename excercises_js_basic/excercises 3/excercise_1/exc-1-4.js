8428092c846ca3d8ee6bef025b1ce81811d5a960
Pleskyjp <Plesky.jp@gmail.com>
2022-04-25 20:26:49 +0200

    completing 1-4 exc

--- excercises_js_basic/excercises 3/excercise_1/exc-1-4.js
+++ excercises_js_basic/excercises 3/excercise_1/exc-1-4.js
@@ -1,7 +1,10 @@
 input5 = [1, 4, 2, 1, 2, 2, 2, 3, 9, 8, 33];
 
 function findUnique(arr) {
-    //TODO
+  return arr
+    .sort(function (a, b) {
+      return a - b;
+    })
+    .filter((value) => arr.filter((element) => element === value).length === 1);
 }
-
 console.log(findUnique(input5)); // [ 3, 4, 8, 9, 33 ]
