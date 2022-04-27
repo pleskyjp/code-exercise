82691fa08a7c86bf08a4eeb6f92c708595c596d4
Pleskyjp <Plesky.jp@gmail.com>
2022-04-28 01:10:59 +0200

    completed 3-2 exc

--- excercises_js_basic/excercises 3/excercise_3/exc-3-2.js
+++ excercises_js_basic/excercises 3/excercise_3/exc-3-2.js
@@ -1,6 +1,11 @@
-
-function acceptThreeFunctions( ) {
-    //TODO
+function acceptThreeFunctions(a, b, c, n = 42) {
+  return [a(n), b(n), c(n)].sort((a, b) => a - b).reverse()[0];
 }
 
-console.log(acceptThreeFunctions(a => a * 2, b => b * 3, c => c * 4)); // 168
+console.log(
+  acceptThreeFunctions(
+    (a) => a * 2,
+    (b) => b * 3,
+    (c) => c * 4
+  )
+); // 168
