92aedf15f8173bb256a9f391543b27356646a047
Pleskyjp <Plesky.jp@gmail.com>
2022-04-28 00:28:26 +0200

    completed 3-1 exc

--- excercises_js_basic/excercises 3/excercise_3/exc-3-1.js
+++ excercises_js_basic/excercises 3/excercise_3/exc-3-1.js
@@ -1,6 +1,5 @@
+var points = [5, 6, 3, 2, 1, 8, 4, 6, -2, 2, -1];
 
-var points = [5,6,3,2,1,8,4,6,-2,2,-1];
-
-//TODO
+points.sort((a, b) => a - b).reverse();
 
 console.log(points); // [ 8, 6, 6, 5, 4, 3, 2, 2, 1, -1, -2 ]
