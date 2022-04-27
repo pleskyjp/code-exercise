71bdc8fee11c3283d7b5cf81541b0e861d63f15f
Pleskyjp <Plesky.jp@gmail.com>
2022-04-28 00:07:20 +0200

    completed 2-4 exc

--- excercises_js_basic/excercises 3/excercise_2/exc-2-4.js
+++ excercises_js_basic/excercises 3/excercise_2/exc-2-4.js
@@ -1,10 +1,35 @@
-
-var csvInput = "1,  Peter,  22;" +
-    "2, Jim, 11,;" +
-    "3, P eeter, 8 ;";
+var csvInput = '1,  Peter,  22;' + '2, Jim, 11,;' + '3, P eeter, 8 ;';
 
 function tranformCsv(string) {
-    //TODO
+  // Splitting string by semicolons to array
+  const splittedString = string.split(';');
+
+  // Removing last empty value in array
+  splittedString.pop();
+
+  // Removing comma on end of string & putting back to array
+  const slicedArray = splittedString[1].slice(0, -1);
+  splittedString.splice(1, 1, slicedArray);
+
+  // Splitting by comma & removing blank spaces
+  const finishedArray = splittedString.map((value) =>
+    value.split(',').map((v) => v.trim())
+  );
+
+  // Final converting
+  const result = finishedArray.map((value) =>
+    value.reduce((a, v, i) => {
+      if (i === 0) {
+        return { ...a, id: Number(v) };
+      } else if (i === 1) {
+        return { ...a, name: v };
+      } else if (i === 2) {
+        return { ...a, age: Number(v) };
+      }
+    }, {})
+  );
+
+  return result;
 }
 
 console.log(tranformCsv(csvInput));
