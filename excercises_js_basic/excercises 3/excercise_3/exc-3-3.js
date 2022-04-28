15fc875913624af7a388925ff0beb870c879e5cf
Pleskyjp <Plesky.jp@gmail.com>
2022-04-28 20:19:04 +0200

    completed 3-3 exc

--- excercises_js_basic/excercises 3/excercise_3/exc-3-3.js
+++ excercises_js_basic/excercises 3/excercise_3/exc-3-3.js
@@ -1,10 +1,11 @@
 let persons = [
-    {firstName: "Jim", lastName: "Jimithy", age: 15},
-    {firstName: "Peter", lastName: "Peterson", age: 25},
-    {firstName: "Carl", lastName: "Carlson", age: 28},
-    {firstName: "Jane", lastName: "Janison", age: 22}
+  { firstName: 'Jim', lastName: 'Jimithy', age: 15 },
+  { firstName: 'Peter', lastName: 'Peterson', age: 25 },
+  { firstName: 'Carl', lastName: 'Carlson', age: 28 },
+  { firstName: 'Jane', lastName: 'Janison', age: 22 },
 ];
 
-//TODO
-
+const oldestPerson = persons.reduce((prev, current) =>
+  prev.age > current.age ? prev : current
+);
 console.log(oldestPerson); // { firstName: 'Carl', lastName: 'Carlson', age: 28 }
