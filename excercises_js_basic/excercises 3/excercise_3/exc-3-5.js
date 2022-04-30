1a96f640ce9e52577c97afdf7dc978ef4be22903
Pleskyjp <Plesky.jp@gmail.com>
2022-04-30 09:35:45 +0200

    completed 3-5 exc

--- excercises_js_basic/excercises 3/excercise_3/exc-3-5.js
+++ excercises_js_basic/excercises 3/excercise_3/exc-3-5.js
@@ -1,8 +1,7 @@
-var points2 = [5,6,3,2,1,8,4,6,-2,2,-55];
+var points2 = [5, 6, 3, 2, 1, 8, 4, 6, -2, 2, -55];
 
 function getNthSmallest(arr, n) {
-    //TODO
+  return arr.sort((a, b) => a - b)[n - 1];
 }
-
 
 console.log(getNthSmallest(points2, 4)); //2
