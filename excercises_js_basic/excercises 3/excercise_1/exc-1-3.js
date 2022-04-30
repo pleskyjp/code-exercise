b8a4ae4e99dc6470cf161618e5c6f4cea9d0c197
Pleskyjp <Plesky.jp@gmail.com>
2022-04-30 10:18:12 +0200

    polishing 1-3 exc

--- excercises_js_basic/excercises 3/excercise_1/exc-1-3.js
+++ excercises_js_basic/excercises 3/excercise_1/exc-1-3.js
@@ -2,24 +2,21 @@ var input4 = [1, 2, 2, 4, 6, 9, 11, 12, 11, 666];
 
 function findModus(arr) {
   let highestOccurrence = 0;
-  let count = [];
+  let count = {};
   let result = [];
 
-  arr.forEach((element) => {
-    if (count[element] === undefined) {
-      count[element] = 1;
-    } else {
-      count[element]++;
-    }
+  arr.forEach((value) => {
+    count[value] === undefined ? (count[value] = 1) : count[value]++;
 
-    if (count[element] > highestOccurrence) {
-      result = [element];
-      highestOccurrence = count[element];
-    } else if (count[element] === highestOccurrence) {
-      result.push(element);
-      highestOccurrence = count[element];
+    if (count[value] > highestOccurrence) {
+      result = [value];
+      highestOccurrence = count[value];
+    } else if (count[value] === highestOccurrence) {
+      result.push(value);
+      highestOccurrence = count[value];
     }
   });
+
   return result;
 }
 
