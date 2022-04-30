dde9c6e6047f91e66e9e15aa307150a3edcf18b9
Pleskyjp <Plesky.jp@gmail.com>
2022-04-30 09:55:32 +0200

    completed 4-1 exc

--- excercises_js_basic/excercises 3/excercise_4/exc-4-1.js
+++ excercises_js_basic/excercises 3/excercise_4/exc-4-1.js
@@ -1,12 +1,16 @@
 var obj = {
-    a: 2,
-    b: "22",
-    c: false,
-    d: "b",
-    f: 3.14,
-    g: 42
+  a: 2,
+  b: '22',
+  c: false,
+  d: 'b',
+  f: 3.14,
+  g: 42,
 };
 
-//TODO
+let count = 0;
+
+Object.keys(obj).forEach((value) =>
+  typeof obj[value] === 'number' ? count++ : null
+);
 
 console.log(count); // 3
