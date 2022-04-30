9fe123fcc4cc8fd4227c7cc5457899182c99e7b7
Pleskyjp <Plesky.jp@gmail.com>
2022-04-30 13:09:33 +0200

    completed 4-3 exc

--- excercises_js_basic/excercises 3/excercise_4/exc-4-3.js
+++ excercises_js_basic/excercises 3/excercise_4/exc-4-3.js
@@ -1,11 +1,13 @@
 var anotherObj = {
-    a: 1,
-    _b: 2,
-    _c: 4,
-    _d: () => {},
-    e: () => {}
+  a: 1,
+  _b: 2,
+  _c: 4,
+  _d: () => {},
+  e: () => {},
 };
 
-//TODO
+Object.keys(anotherObj).forEach((value) => {
+  value.startsWith('_') ? delete anotherObj[value] : null;
+});
 
 console.log(anotherObj); //{ a: 1, e: [Function: e] }
