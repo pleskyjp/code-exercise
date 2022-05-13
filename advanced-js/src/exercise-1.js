808b1105b35dafc911e40d9b0d167a537a7d066c
pleskyjp <plesky.jp@gmail.com>
2022-05-13 19:39:55 +0200

    completed exc-1

--- advanced-js/src/exercise-1.js
+++ advanced-js/src/exercise-1.js
@@ -1,11 +1,23 @@
 function getInfo() {
-  var results = [];
-  // TODO
-  values.forEach((value) => {
-    // TODO
-  });
-  return results
-}
+  const values = Array.from(arguments);
+  let results = [];
 
+  let index = 0;
+  values.forEach((value) => {
+    const isFalsy = !value;
+    const type = typeof value;
+    const StringLength = String(value).length;
+
+    results.push({
+      isFalsy,
+      type,
+      StringLength,
+      index,
+    });
+
+    index++;
+  });
+  return results;
+}
 
 module.exports = getInfo;
